ea0e5a7bd2b09c61c0c631ce4ae3e56836427cd2
zhuyu369 <148209104+zhuyu369@users.noreply.github.com>
2025-12-12 18:41:51 +0800

    Delete SCAFNet directory

--- SCAFNet/models/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-logger = logging.getLogger('base')
-
-
-def create_model(opt):
-    from .Fusion_model import SCANet as M
-    m = M(opt)
-    logger.info('Fusion Model [{:s}] is created.'.format(m.__class__.__name__))
-    return m
